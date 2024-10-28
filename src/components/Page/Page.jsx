16cbdd751271fffbd2a5c16a149c5aa8b0911b6f
michalk1924 <michalk1924@gmail.com>
2024-10-28 12:12:09 +0200

    add loading

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -2,31 +2,38 @@ import React, { useEffect, useState } from 'react';
 import { getItems } from '../../service/store';
 import styles from './styles.module.css'
 import { getFromStorage, saveToStorage } from '../../service/lib/storage';
+import { ClipLoader } from 'react-spinners';
 
 const oneMinute = 60 * 1000;
 
 function Page({ catgory }) {
 
     const [itemsArray, setItemsArray] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const savedTimefromStorage = getFromStorage(`savedTime${catgory}`)
-        if (savedTimefromStorage) {
-            const storedTime = parseInt(savedTimefromStorage, 10);
-            const currentTime = Date.now();
-            if (currentTime - storedTime > oneMinute) {
-               getItemSWithFetch();
-            } else {
-                console.log('storage');
-                setItemsArray(getFromStorage(catgory));
+
+        async function fetchData() {
+            setLoading(true);
+            const savedTimefromStorage = getFromStorage(`savedTime${catgory}`)
+            if (savedTimefromStorage) {
+                const storedTime = parseInt(savedTimefromStorage, 10);
+                const currentTime = Date.now();
+                if (currentTime - storedTime > oneMinute) {
+                    await getItemsWithFetch();
+                } else {
+                    setItemsArray(getFromStorage(catgory));
+                }
             }
-        }
-        else {
-            getItemSWithFetch();
+            else {
+                await getItemsWithFetch();
+            }
+            setLoading(false);
         }
 
-        function getItemSWithFetch() {
-            console.log('fetch');
+        fetchData();
+
+        async function getItemsWithFetch() {
             getItems(catgory).then(data => {
                 setItemsArray(data);
                 saveToStorage(catgory, data);
@@ -40,17 +47,23 @@ function Page({ catgory }) {
 
     return (
         <div>
-            <h2>{catgory.toUpperCase()}</h2>
-            <div className={styles.prodContainer}>
-                {itemsArray.map((product, index) =>
-                    <div key={index} className={styles.prod}>
-                        <h4>{product.title}</h4>
-                        <p>Price: ${product.price}</p>
-                        <img src={product.image} alt={product.title} />
-                    </div>
-                )
-                }
-            </div>
+            {!loading && <div>
+                <h2>{catgory.toUpperCase()}</h2>
+                <div className={styles.prodContainer}>
+                    {itemsArray.map((product, index) =>
+                        <div key={index} className={styles.prod}>
+                            <h4>{product.title}</h4>
+                            <p>Price: ${product.price}</p>
+                            <img src={product.image} alt={product.title} />
+                        </div>
+                    )
+                    }
+                </div>
+            </div>}
+            {loading &&
+                <div>
+                    <ClipLoader color="orange" loading={loading} size={50} />
+                </div>}
         </div>
     )
 }
